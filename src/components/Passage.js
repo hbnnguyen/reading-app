2f304bb71ee3637f4cec2fa56e240d32eff6b640
Hannah Nguyen <hnguyen160801@gmail.com>
2023-10-15 12:34:42 -0400

    add right and left click events for Passage

--- src/components/Passage.js
+++ src/components/Passage.js
@@ -1,12 +1,12 @@
 import "./Passage.css";
 
 const Selectable = ({ word }) => {
-
   const handleClick = (e) => {
-    if (e.type === 'click') {
-      console.log('Left click');
-    } else if (e.type === 'contextmenu') {
+    if (e.type === 'click') { // left click
+      // when left click on a selectable word, the utterance should stop and begin again on said word.
+    } else if (e.type === 'contextmenu') { // right click
       e.preventDefault()
+      // when right click on a selectable, make a call to dictionary api to get the definition
       console.log('Right click');
     }
   }
@@ -15,7 +15,8 @@ const Selectable = ({ word }) => {
     <span
       className="selectable"
       onClick={handleClick}
-      onContextMenu={handleClick}>
+      onContextMenu={handleClick}
+    >
         {word}
     </span>
   )
