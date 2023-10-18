5879992d812b17b20ab2a34e58236977766b25c2
Brian Farrell <farrelb6@tcd.ie>
2023-10-17 23:07:02 -0700

    Addeed page for user inputted books

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import TextToSpeech from './components/TextToSpeech';
 // import Passage from './components/Passage';
 import Main from './pages/Main';
+import WriteBook from './pages/WriteBook';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path='/read' element={<Main />} />
+        <Route path='/write' element={<WriteBook />} />
       </Routes>
     </>
   );
