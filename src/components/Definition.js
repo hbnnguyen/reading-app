cc74d3cfafa47dd9d831f34e2ad5c5407181c05f
Brian Farrell <farrelb6@tcd.ie>
2023-10-18 17:38:53 -0700

    Using GPT to generate word definitions

--- src/components/Definition.js
+++ src/components/Definition.js
@@ -3,46 +3,56 @@ import React, { useCallback, useEffect, useState } from 'react';
 const Definition = ({ word, wordDefinitions }) => {
   const [dataToDisplay, setDataToDisplay] = useState([])
 
-  const meanings = wordDefinitions[0].meanings;
-  const toDisplay = useCallback(() => {
-    if (meanings) {
-      const array = [];
-      meanings.forEach((meaning, index) => {
-        array[index] = {};
-        array[index].partOfSpeech = meaning.partOfSpeech;
-        array[index].definitions = [];
-        meaning.definitions.forEach((d) => {
-          array[index].definitions.push(d.definition);
-        });
-      });
-      return array;
-    }
-  }, [meanings]);
-
-  useEffect(() => {
-    const array = toDisplay();
-    setDataToDisplay(array)
-    console.log(array);
-  }, [word, toDisplay]);
-
-  return (
+   useEffect(() => {
+    setDataToDisplay(wordDefinitions)
+    console.log(wordDefinitions);
+  });
+  return(
     <div>
       <h2>{word}</h2>
-      <div>
-      {dataToDisplay &&
-          dataToDisplay.map((data, index) => (
-            <div key={word+"Meaning"+index}>
-              <h3>Part of Speech: {data.partOfSpeech}</h3>
-              <ul>
-                {data.definitions.map((definition, definitionIndex) => (
-                  <li key={word+"Def"+definitionIndex}>{definition}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-      </div>
+      <p> {dataToDisplay}</p>
     </div>
-  );
+  )
+  // const meanings = wordDefinitions[0].meanings;
+  // const toDisplay = useCallback(() => {
+  //   if (meanings) {
+  //     const array = [];
+  //     meanings.forEach((meaning, index) => {
+  //       array[index] = {};
+  //       array[index].partOfSpeech = meaning.partOfSpeech;
+  //       array[index].definitions = [];
+  //       meaning.definitions.forEach((d) => {
+  //         array[index].definitions.push(d.definition);
+  //       });
+  //     });
+  //     return array;
+  //   }
+  // }, [meanings]);
+
+  // useEffect(() => {
+  //   const array = toDisplay();
+  //   setDataToDisplay(array)
+  //   console.log(array);
+  // }, [word, toDisplay]);
+
+  // return (
+  //   <div>
+  //     <h2>{word}</h2>
+  //     <div>
+  //     {dataToDisplay &&
+  //         dataToDisplay.map((data, index) => (
+  //           <div key={word+"Meaning"+index}>
+  //             <h3>Part of Speech: {data.partOfSpeech}</h3>
+  //             <ul>
+  //               {data.definitions.map((definition, definitionIndex) => (
+  //                 <li key={word+"Def"+definitionIndex}>{definition}</li>
+  //               ))}
+  //             </ul>
+  //           </div>
+  //         ))}
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default Definition;
